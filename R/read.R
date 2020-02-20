0580d7a8e7822a1692a5b8a2c529ed2c616a7b74
Brandmaier <brandmaier@mpib-berlin.mpg.de>
2020-02-20 13:18:48 +0100

    here is a suggestion to improve reading in data from files!

--- R/read.R
+++ R/read.R
@@ -3,5 +3,6 @@ pacman::p_load(here, tidyverse, fs)
 
 #----read----
 results_files <- dir_ls(here("data", "raw"))
-results <- map_dfr(results_files, read.csv, .id = "id")
+# I think it would be better to use readr package here
+results <- map_dfr(results_files, readr::read_csv, .id = "id")
 results <- mutate(results, id = path_ext_remove(path_file(id)))
