da49ed2082659838c6448e0387995ccb00b40195
Brandmaier <brandmaier@mpib-berlin.mpg.de>
2020-02-20 13:27:00 +0100

    my code is better now

--- R/read.R
+++ R/read.R
@@ -4,5 +4,5 @@ pacman::p_load(here, tidyverse, fs)
 #----read----
 results_files <- dir_ls(here("data", "raw"))
 # I think it would be better to use readr package here
-results <- map_dfr(results_files, readr::read_csv, .id = "id")
+results_better <- map_dfr(results_files, readr::read_csv, .id = "id")
 results <- mutate(results, id = path_ext_remove(path_file(id)))
