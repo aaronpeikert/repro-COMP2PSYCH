293ea620501396d153d0e553f2daf5f3f55e991e
Anna Thoma <thoma@mpib-berlin.mpg.de>
2020-02-20 13:50:17 +0100

    This will be a freaking mess

--- R/Annas.R
+++ R/Annas.R
@@ -2,4 +2,11 @@ if(!requireNamespace("pacman"))install.packages("pacman")
 pacman::p_load(here, tidyverse)
 
 source(here("R", "read.R"))
-ggplot(results, aes(diff, time, group = diff)) + geom_boxplot() 
+results$diff = as.factor(results$diff)
+
+ggplot(results, aes(diff, time, group = diff, color=diff, shape=diff)) + 
+  geom_boxplot() + 
+  theme_minimal() +
+  ggtitle("The Annas Plot")+
+  scale_y_continuous(name="Time", breaks=c(0,5,10,15,20,25,30,35,50), labels=c(0,5,10,15,20,25,30,35,50), limits = c(0,40))+
+  scale_x_discrete(name="Difference")
