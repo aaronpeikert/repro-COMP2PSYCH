6b4c8dbfbae280c8af637d04515459cda39bfcfd
Jane Doe <jane@example.org>
2020-02-20 13:53:16 +0100

    my code sucks

--- R/Annas.R
+++ R/Annas.R
@@ -2,4 +2,6 @@ if(!requireNamespace("pacman"))install.packages("pacman")
 pacman::p_load(here, tidyverse)
 
 source(here("R", "read.R"))
-ggplot(results, aes(diff, time, group = diff)) + geom_boxplot() 
+ggplot(results, aes(diff, time, group = diff)) + geom_boxplot() +
+  theme_bw()
+
