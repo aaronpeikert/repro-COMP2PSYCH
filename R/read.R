b55a6bae3a2124d7b317f65df791a3e4aba8c109
Aaron Peikert <aaron.peikert@posteo.de>
2020-02-20 13:19:13 +0100

    use fread

--- R/read.R
+++ R/read.R
@@ -3,5 +3,5 @@ pacman::p_load(here, tidyverse, fs)
 
 #----read----
 results_files <- dir_ls(here("data", "raw"))
-results <- map_dfr(results_files, read.csv, .id = "id")
+results <- map_dfr(results_files, data.table::fread, .id = "id")
 results <- mutate(results, id = path_ext_remove(path_file(id)))
