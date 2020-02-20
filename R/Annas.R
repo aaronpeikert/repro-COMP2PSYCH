5508a5e29bc69e324c245b93ac58c6d0cc6fe9f1
annariha <rihaanna@student.hu-berlin.de>
2020-02-20 13:54:38 +0100

    added some features to the plot

--- R/Annas.R
+++ R/Annas.R
@@ -2,4 +2,10 @@ if(!requireNamespace("pacman"))install.packages("pacman")
 pacman::p_load(here, tidyverse)
 
 source(here("R", "read.R"))
-ggplot(results, aes(diff, time, group = diff)) + geom_boxplot() 
+
+results <- results %>% mutate(diff = factor(diff))
+
+ggplot(results, aes(diff, time, group = diff, color = diff)) + geom_point() + 
+  geom_boxplot() +
+  scale_y_continuous(trans='log10') + 
+  theme_bw()
